002cafc9b13a5e396e00d22b8a50b1b09f2e6db0
Nathaniel van Diepen <eeems@omnimaga.org>
2013-04-08 21:07:22 -0600

    More cleanup

--- omnomirc_www/brokenphp.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Broken PHP file for 500.html testing
-faij39t04jgqewovdsml
-dsve]g!R#*T*()E)G)#I(@*$()!%*#!(%()$@*^()*(
-?>
--- omnomirc_www/callback.php
+++
@@ -1,4 +0,0 @@
-<?PHP
-header('Content-type: text/javascript');
-echo "parent.signCallback('".base64_url_decode($_GET['signature'])."','".base64_url_decode($_GET['nick'])."');"
-?>
