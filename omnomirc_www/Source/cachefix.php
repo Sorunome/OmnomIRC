e65e7a05316579c51c3cc62598f84025b0bf7971
Nathaniel van Diepen <eeems13@gmail.com>
2013-05-07 15:58:59 -0400

    Restructure and deletion of unneeded php code

--- omnomirc_www/Source/cachefix.php
+++
@@ -1,7 +0,0 @@
-<?PHP
-	header("Last-Modified: Thu, 01-Jan-1970 00:00:01 GMT");
-	header("Cache-Control: no-store, no-cache, must-revalidate");
-	header("Cache-Control: post-check=0, pre-check=0", false);
-	header("Pragma: no-cache");
-	date_default_timezone_set("America/Denver"); //Leave me the fuck alone.
-?>
