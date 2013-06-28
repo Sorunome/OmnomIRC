f7384bdea8db0add851db73c7b7b23026a1474ce
Nathaniel van Diepen <eeems@omnimaga.org>
2013-06-28 15:26:40 -0600

    More formatting fixes

--- shims/smf/shim.php
+++ shims/smf/shim.php
@@ -1,13 +1,13 @@
 <?PHP
 	$encriptKeyToUse = "key from Config.php (created while installation)";
 	function base64_url_encode($input){
-		return strtr(base64_encode($input), '+/=', '-_,');
+		return strtr(base64_encode($input),'+/=','-_,');
 	}
 	function base64_url_decode($input){
-		return base64_decode(strtr($input, '-_,', '+/=')); 
+		return base64_decode(strtr($input,'-_,','+/=')); 
 	}
 	$ssi_guest_access = true;
-	@require(dirname(__FILE__) . '/SSI.php');
+	@require(dirname(__FILE__).'/SSI.php');
 	ob_start();
 	if(isset($_GET['txt'])){
 		header('Content-type: text/plain');
@@ -19,7 +19,7 @@
 		$signature = "";
 	}else{
 		$nick = $user_info['name'];
-		$signature = base64_url_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256 , $encriptKeyToUse , $nick , MCRYPT_MODE_ECB));
+		$signature = base64_url_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256,$encriptKeyToUse,$nick,MCRYPT_MODE_ECB));
 	}
 	ob_end_clean();
 	if(isset($_GET['txt'])){
