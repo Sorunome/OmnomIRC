fc516d9cb4278b79f121cedbb85db65e05eeb2a5
Nathaniel van Diepen <eeems@omnimaga.org>
2014-05-23 12:44:10 -0600

    Fixed initscript naming.

--- initscript.sh
+++ initscript.sh
@@ -3,11 +3,11 @@
 user="sorunome"
 group=$user
 home=/home/$user/omnomirc
-pid=/run/omnomirc_curid
+curid=/run/omnomirc_curid
 ####### Init ########
-echo "0" > $pid
-chown $user:$group $pid
-chmod 777 $pid
+echo "0" > $curid
+chown $user:$group $curid
+chmod 777 $curid
 cd $home
 su -c "./MrOmnomIRC &> /dev/null &" $user
 su -c "./MrTopicBot &> /dev/null &" $user
