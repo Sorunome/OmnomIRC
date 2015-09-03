66e872a54b45d1cb0fc534ab9903b99a647791e2
Sorunome <mail@sorunome.de>
2015-09-03 22:18:28 +0200

    fixed one pixel off bug

--- omnomirc_www/omnomirc.js
+++ omnomirc_www/omnomirc.js
@@ -2386,7 +2386,7 @@ oirc = (function(){
 									sending = false;
 								});
 							}
-							if(s.search('goo.gl/QMET')!=-1 || s.search('oHg5SJYRHA0')!=-1 || s.search('dQw4w9WgXcQ')!=-1){
+							/*if(s.search('goo.gl/QMET')!=-1 || s.search('oHg5SJYRHA0')!=-1 || s.search('dQw4w9WgXcQ')!=-1){
 								$('<div>')
 									.css({
 										position:'absolute',
@@ -2396,7 +2396,7 @@ oirc = (function(){
 									})
 									.html('<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0"><param name="movie" value="http://134.0.27.190/juju/i-lost-the-ga.me/rickroll.swf"><param name="quality" value="high"><embed src="http://134.0.27.190/juju/i-lost-the-ga.me/rickroll.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash"></embed></object>')
 									.appendTo('body');
-							}
+							}*/
 						}
 					}
 				};
