522c5938f9c5e683efdb4ef9f61cc16a68a0b250
Sorunome <mail@sorunome.de>
2015-10-25 18:37:37 +0100

    basic smf settings

---
+++ smf_mod/oirc.english.php
@@ -0,0 +1,20 @@
+<?php
+// OmnomIRC mod language
+
+$txt['oirc_admin'] = 'OmnomIRC Administration';
+$txt['oirc_topics'] = 'Notify new topics';
+$txt['oirc_posts'] = 'Notify new posts';
+$txt['oirc_edits'] = 'Notify post edits';
+$txt['oirc_notification_notice'] = 'The following settings all use these replacement codes:<br>
+<ul>
+	<li>{COLOR} - IRC color code</li>
+	<li>{NAME} - poster name</li>
+	<li>{TOPIC} - posting topic</li>
+	<li>{SUBJECT} - posting subject</li>
+	<li>{TOPICID} - topic id</li>
+	<li>{POSTID} - post id (not for new topic)</li>
+</ul>';
+$txt['oirc_topicnotification'] = 'Notification message for new topics';
+$txt['oirc_postnotification'] = 'Notification message for new posts';
+$txt['oirc_editnotification'] = 'Notification message for post edits';
+?>
