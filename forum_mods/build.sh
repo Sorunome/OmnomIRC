f0eb82efd78ceff86354162f480caf84c34d33d6
Sorunome <mail@sorunome.de>
2015-11-01 14:42:26 +0100

    folder structure

---
+++ forum_mods/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd smf
+./build.sh
+cd ..
--- forum_mods/smf/build.sh
+++ forum_mods/smf/build.sh
@@ -3,9 +3,9 @@ echo "Generating SMF mod ..."
 rm omnomirc_smf.tar.gz
 sed 's/<file name=\"\$sourcedir\/Subs-Post.php\">/<file name=\"\$boarddir\/mobiquo\/include\/Subs-Post.php\" error=\"skip\">/' install.xml > install_tapatalk.xml
 mkdir checkLogin
-cp ../checkLogin/index.php checkLogin
-sed 's/generic/smf/' ../checkLogin/config.json.php > checkLogin/config.json.php
-cp ../checkLogin/hook-smf.php checkLogin
+cp ../../checkLogin/index.php checkLogin
+sed 's/generic/smf/' ../../checkLogin/config.json.php > checkLogin/config.json.php
+cp ../../checkLogin/hook-smf.php checkLogin
 tar -zcvf omnomirc_smf.tar.gz *.xml *.php checkLogin/* > /dev/null
 rm install_tapatalk.xml
 rm -r checkLogin
