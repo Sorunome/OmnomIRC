ca9b0c0e885d33a777d9d88e1961352ef2510193
Sorunome <mail@sorunome.de>
2015-11-22 23:08:53 +0100

    had to adapt build-script

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ UGLIFYOPTIONS="-m --comments -v"
 
 echo "minifying omnomirc.js ..."
 sed 's/admin\.js/admin\.min\.js/' src/omnomirc.js > src/omnomirc.js.tmp
-uglifyjs $UGLIFYOPTIONS src/omnomirc.js.tmp -o omnomirc_www/omnomirc.min.js
+uglifyjs $UGLIFYOPTIONS src/omnomirc.js.tmp -o html/omnomirc_www/omnomirc.min.js
 rm src/omnomirc.js.tmp
 echo "minifying admin.js ..."
-uglifyjs $UGLIFYOPTIONS src/admin.js -o omnomirc_www/admin.min.js
+uglifyjs $UGLIFYOPTIONS src/admin.js -o html/omnomirc_www/admin.min.js
 
 if [ "$1" = "all" ]; then
 	cd forum_mods
